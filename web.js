5554fa74f57664285ac0d72d9f5e71ebcb779693
Tiago Conti <tgconti@gmail.com>
2013-07-12 14:23:34 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var  fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var data = fs.readFileSync('index.html', 'ascii');
+  var buffer = new Buffer(data);
+  var text = buffer.toString(); 
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
